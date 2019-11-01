8502859c1e86f727ed13a64366263b0a942e1d97
lankaukk <47405698+lankaukk@users.noreply.github.com>
2019-11-01 18:24:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+  var greeting = 'bonjour' ;
+
+  var x = 3;
+  var y = 4;
+  console.log(x * y);
+
+  $('.red').on('click', function(){
+  		$('.red').toggleClass('bigred');
+  });
+
+  $('.red2').on('click', function(){
+  		$('.red2').toggleClass('bigred2');
+  });
+
+  $('.orange').on('click', function(){
+  		$('.orange').toggleClass('bigorange');
+  });
+
+  $('.orange2').on('click', function(){
+  		$('.orange2').toggleClass('bigorange2');
+  });
+
+  $('.yellow').on('click', function(){
+  		$('.yellow').toggleClass('bigyellow');
+  });
+
+  $('.green').on('click', function(){
+  		$('.green').toggleClass('biggreen');
+  });
+
+  $('.blue').on('click', function(){
+  		$('.blue').toggleClass('bigblue');
+  });
+
+  $('.purple').on('click', function(){
+  		$('.purple').toggleClass('bigpurple');
+  });
+
+  $('.violet').on('click', function(){
+  		$('.violet').toggleClass('bigviolet');
+  });
+
+  $('.yellow2').on('click', function(){
+  		$('.yellow2').toggleClass('bigyellow2');
+  });
+
+  $('.green2').on('click', function(){
+  		$('.green2').toggleClass('biggreen2');
+  });
+
+  $('.blue2').on('click', function(){
+  		$('.blue2').toggleClass('bigblue2');
+  });
+
+  $('.purple2').on('click', function(){
+  		$('.purple2').toggleClass('bigpurple2');
+  });
+
+  $('.violet2').on('click', function(){
+  		$('.violet2').toggleClass('bigviolet2');
+  });
+
+});
